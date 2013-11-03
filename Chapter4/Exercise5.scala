597031f28d7fa3e6a4dc530164d280d0d906fb51
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-11-03 21:45:13 +0000

    Got chapter 4 exercise 5 working (2 different version of the sequence function)

--- Chapter4/Exercise5.scala
+++ Chapter4/Exercise5.scala
@@ -2,17 +2,50 @@ import fpinscala.datastructures._
 
 object Exercise5 {
 
-	private def sequence[A](as: List[Option[A]]): Option[List[A]] = {
-		None
+	private def sequence1[A](as: List[Option[A]]): Option[List[A]] = {
+
+		def loop[A](as: List[Option[A]], bs: List[A]): Option[List[A]] =
+			as match {
+				case head :: tail => head match {
+					case Some(a) => loop(tail, a :: bs)
+					case _ => None
+				}
+				case Nil => Some(bs.reverse)
+			}
+
+		loop(as, Nil)
 	}
 
+	private def sequence2[A](as: List[Option[A]]): Option[List[A]] =
+		try {
+			Some(as.foldRight(Nil: List[A])((ao: Option[A], bs: List[A]) => {
+				ao match {
+					case Some(a) => a :: bs
+					case _ => throw new Exception("Found a None item")
+				}
+			}))
+		}
+		catch {
+			case e: Exception => None
+		}
+
 	private def showResult[A](as: List[Option[A]]): Unit = {
-		val r = sequence(as)
-		println("sequence(%s): %s".format(as, r))
+		val r1 = sequence1(as)
+		println("sequence1(%s): %s".format(as, r1))
+		val r2 = sequence2(as)
+		println("sequence2(%s): %s".format(as, r2))
 	}
 
 	def main(args: Array[String]): Unit = {
+
 		showResult(List(Some(1), Some(2), Some(3)))
+		showResult(List(Some(1.0), Some(2.0), Some(3.0)))
+		showResult(List(Some("A"), Some("B"), Some("C")))
+
+		showResult(Nil: List[Option[Int]])
+		showResult(Nil: List[Option[Double]])
+		showResult(Nil: List[Option[String]])
+
 		showResult(List(None, Some(2), Some(3)))
 		showResult(List(Some(1), None, Some(3)))
 		showResult(List(Some(1), Some(2), None))
